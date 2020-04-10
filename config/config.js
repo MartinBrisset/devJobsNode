7b56bc2a8eac97cdb190555923a8e99167c316c6
martin <brissetmartin@gmail.com>
2020-04-10 10:03:24 -0300

    agregando variables env

---
+++ config/config.js
@@ -0,0 +1,11 @@
+process.env.PORT = process.env.PORT || 5000;
+
+process.env.URLDB = process.env.URLDB ;
+
+process.env.SECRETO = process.env.SECRETO ;
+process.env.KEY = process.env.KEY ;
+
+process.env.MAIL_USER = process.env.MAIL_USER || '1cc34706a397ab';
+process.env.MAIL_PASS = process.env.MAIL_PASS || 'edb0c33381fe1c';
+process.env.MAIL_HOST = process.env.MAIL_HOST || 'smtp.mailtrap.io';
+process.env.MAIL_PORT = process.env.MAIL_PORT || '2525';
